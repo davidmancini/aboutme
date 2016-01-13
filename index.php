04b8909cba42932d0091a2cd6b4f26ae9c4f30de
David Mancini <mancini.david@gmail.com>
2016-01-13 11:46:07 -0700

    Added content to index.php

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>About David</title>
+	</head>
+	<body>
+		<h1>About David</h1>
+		<h2>Work</h2>
+			<ul>
+				<li>Paramedic</li>
+					I've been a paramedic since 2009.  I love working on the ambulance and responding to 911 calls.  It's a great feeling to be able to say that I played a role in saving someobody's life.
+				<li>Nurse</li>
+					I recently graduated from nursing school as a Registered Nurse.  Instead of getting a job as a nurse, I decided to come here and play with computers.
+				<li>CPR Instructor</li>
+					I have been teaching medical courses for a long time, beginning with Lifeguarding.  I own and operate a CPR training center in San Antonio.  I have two other instructors.  Hopefully they're actually working while I'm here for 10 weeks......
+			</ul><br>
+		<h2>Play</h2>
+			<ul>
+				<li>Morgan</li>
+					Morgan is my dog.  She is a 4-year-old mixed-breed dog.  I was lucky enough to be able to bring her with me while I'm here in New Mexico!  I'd put a photo on here of her, but we haven't covered uploading photos yet. :(
+				<li>Swimming</li>
+					I think I'm half fish because I absoultely love swimming.  My summers are pretty much spent at the river or at a pool.  I've been a lifeguard, taught swim lessons, and won a gold medal in the olympics.  Just kidding--you probably didn't read this far, anyway.
+			</ul><br>
+		<strong>Thanks for visiting!</strong>
+	</body>
+</html>
