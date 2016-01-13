d2c83ebff1965538c34332d8cf6a0c387036f19b
David Mancini <mancini.david@gmail.com>
2016-01-13 15:16:45 -0700

    Changed CSS to look better.

--- index.php
+++ index.php
@@ -8,8 +8,9 @@
 	</head>
 	<body>
 		<h1><div class="center">About David</div></h1>
+
+		<h2>Work</h2>
 		<div class="aboutbox">
-			<h2>Work</h2>
 				<ul>
 					<li><strong>Paramedic</strong></li>
 						I've been a paramedic since 2009.  I love working on the ambulance and responding to 911 calls.  It's a great feeling to be able to say that I played a role in saving someobody's life.
@@ -18,9 +19,10 @@
 					<li><strong>CPR Instructor</strong></li>
 						I have been teaching medical courses for a long time, beginning with Lifeguarding.  I own and operate a CPR training center in San Antonio.  I have two other instructors.  Hopefully they're actually working while I'm here for 10 weeks......
 				</ul><br>
-		</div>
+		</div><br>
+
+		<h2>Play</h2>
 		<div class="aboutbox">
-			<h2>Play</h2>
 				<ul>
 					<li><strong>Morgan</strong></li>
 						Morgan is my dog.  She is a 4-year-old mixed-breed dog.  I was lucky enough to be able to bring her with me while I'm here in New Mexico!<br>
@@ -29,6 +31,7 @@
 						I think I'm half fish because I absoultely love swimming.  My summers are pretty much spent at the river or at a pool.  I've been a lifeguard, taught swim lessons, and won a gold medal in the olympics.  Just kidding--you probably didn't read this far, anyway.
 				</ul>
 		</div><br><br>
+
 		<footer>
 			<div class="center">Thanks for visiting!</div><br>
 		</footer>
