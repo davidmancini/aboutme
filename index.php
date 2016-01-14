7b688328c6fc242b9b9254239928072889450306
David Mancini <mancini.david@gmail.com>
2016-01-13 20:34:13 -0700

    Fixed syntax errors in index.php regarding <div> inside other classes.

--- index.php
+++ index.php
@@ -7,39 +7,42 @@
 		<link href='https://fonts.googleapis.com/css?family=Indie+Flower|Raleway' rel='stylesheet' type='text/css'>
 	</head>
 	<body>
-		<h1><div class="center">About David</div></h1>
+		<h1 class="center">About David</h1>
 
 		<h2>Work</h2>
-		<div class="aboutbox">
+			<div class="aboutbox">
 				<ul>
 					<li><strong>Paramedic</strong><br>
-						I've been a paramedic since 2009.  I love working on the ambulance and responding to 911 calls.  It's a great feeling to be able to say that I played a role in saving someobody's life.</li><br>
+						I've been a paramedic since 2009.  I love working on the ambulance and responding to 911 calls.  It's a great feeling to be able to say that I played a role in saving someobody's life.</li>
 					<li><strong>Nurse</strong><br>
-						I recently graduated from nursing school as a Registered Nurse.  Instead of getting a job as a nurse, I decided to come here and play with computers.</li><br>
+						I recently graduated from nursing school as a Registered Nurse.  Instead of getting a job as a nurse, I decided to come here and play with computers.</li>
 					<li><strong>CPR Instructor</strong><br>
 						I have been teaching medical courses for a long time, beginning with Lifeguarding.  I own and operate a CPR training center in San Antonio.  I have two other instructors.  Hopefully they're actually working while I'm here for 10 weeks......</li>
 				</ul>
-		</div><br>
+			</div><br>
 
 		<h2>Play</h2>
-		<div class="aboutbox">
+			<div class="aboutbox">
+				<table>
+					<tr>
+						<td>
+							<ul>
+								<li><strong>Morgan</strong><br>Morgan is my dog.  She is a 4-year-old mixed-breed dog.  I was lucky enough to be able to bring her with me while I'm here in New Mexico!</li>
+							</ul>
+						</td>
+						<td><img src="images/morgan.jpg" alt="Morgan" height="240" width="320"></td>
+					</tr>
+				</table>
 				<ul>
-					<table>
-						<tr>
-							<td><li><strong>Morgan</strong><br>Morgan is my dog.  She is a 4-year-old mixed-breed dog.  I was lucky enough to be able to bring her with me while I'm here in New Mexico!</ul></td>
-							<!-- <div class="center"><img src="images/morgan.jpg" alt="Morgan" width="320" height="240"></div><br> -->
-							<td><img src="images/morgan.jpg" alt="Morgan" width="320" height="240"><br></td>
-						</tr>
-					</table>
 					<li><strong>Swimming</strong><br>
-						I think I'm half fish because I absoultely love swimming.  My summers are pretty much spent at the river or at a pool.  I've been a lifeguard, taught swim lessons, and won a gold medal in the olympics.  Just kidding--you probably didn't read this far, anyway.</li><br>
+						I think I'm half fish because I absoultely love swimming.  My summers are pretty much spent at the river or at a pool.  I've been a lifeguard, taught swim lessons, and won a gold medal in the olympics.  Just kidding--you probably didn't read this far, anyway.</li>
 					<li><strong>Photography</strong><br>
-						I have a pretty good camera which takes high-quality photos.  I like to say that I'm an amerature photographer, but it's probably just because of the good camera.  Skills aside, I've taken some cool photos!</li><br>
+						I have a pretty good camera which takes high-quality photos.  I like to say that I'm an amerature photographer, but it's probably just because of the good camera.  Skills aside, I've taken some cool photos!</li>
 				</ul>
-		</div><br><br>
+			</div><br><br>
 
 		<footer>
-			<div class="center">Thanks for visiting!</div><br>
+			<div class="center">Thanks for visiting!</div>
 		</footer>
 	</body>
 </html>
