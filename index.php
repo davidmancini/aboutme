1d757c510769dd2ae889ea404663d2487d9c1d15
David Mancini <mancini.david@gmail.com>
2016-01-13 17:01:17 -0700

    Added a table to format Morgan's photo.  Also added a Photography section.

--- index.php
+++ index.php
@@ -12,25 +12,30 @@
 		<h2>Work</h2>
 		<div class="aboutbox">
 				<ul>
-					<li><strong>Paramedic</strong></li>
-						I've been a paramedic since 2009.  I love working on the ambulance and responding to 911 calls.  It's a great feeling to be able to say that I played a role in saving someobody's life.
-					<li><strong>Nurse</strong></li>
-						I recently graduated from nursing school as a Registered Nurse.  Instead of getting a job as a nurse, I decided to come here and play with computers.
-					<li><strong>CPR Instructor</strong></li>
-						I have been teaching medical courses for a long time, beginning with Lifeguarding.  I own and operate a CPR training center in San Antonio.  I have two other instructors.  Hopefully they're actually working while I'm here for 10 weeks......
-				</ul><br>
+					<li><strong>Paramedic</strong><br>
+						I've been a paramedic since 2009.  I love working on the ambulance and responding to 911 calls.  It's a great feeling to be able to say that I played a role in saving someobody's life.</li><br>
+					<li><strong>Nurse</strong><br>
+						I recently graduated from nursing school as a Registered Nurse.  Instead of getting a job as a nurse, I decided to come here and play with computers.</li><br>
+					<li><strong>CPR Instructor</strong><br>
+						I have been teaching medical courses for a long time, beginning with Lifeguarding.  I own and operate a CPR training center in San Antonio.  I have two other instructors.  Hopefully they're actually working while I'm here for 10 weeks......</li>
+				</ul>
 		</div><br>
 
 		<h2>Play</h2>
 		<div class="aboutbox">
 				<ul>
-					<li><strong>Morgan</strong></li>
-						Morgan is my dog.  She is a 4-year-old mixed-breed dog.  I was lucky enough to be able to bring her with me while I'm here in New Mexico!<br>
-						<div class="center"><img src="images/morgan.jpg" alt="Morgan" width="320" height="240"></div>
-					<li><strong>Swimming</strong></li>
-						I think I'm half fish because I absoultely love swimming.  My summers are pretty much spent at the river or at a pool.  I've been a lifeguard, taught swim lessons, and won a gold medal in the olympics.  Just kidding--you probably didn't read this far, anyway.
+					<table>
+						<tr>
+							<td><li><strong>Morgan</strong><br>Morgan is my dog.  She is a 4-year-old mixed-breed dog.  I was lucky enough to be able to bring her with me while I'm here in New Mexico!</ul></td>
+							<!-- <div class="center"><img src="images/morgan.jpg" alt="Morgan" width="320" height="240"></div><br> -->
+							<td><img src="images/morgan.jpg" alt="Morgan" width="320" height="240"><br></td>
+						</tr>
+					</table>
+					<li><strong>Swimming</strong><br>
+						I think I'm half fish because I absoultely love swimming.  My summers are pretty much spent at the river or at a pool.  I've been a lifeguard, taught swim lessons, and won a gold medal in the olympics.  Just kidding--you probably didn't read this far, anyway.</li><br>
+					<li><strong>Photography</strong><br>
+						I have a pretty good camera which takes high-quality photos.  I like to say that I'm an amerature photographer, but it's probably just because of the good camera.  Skills aside, I've taken some cool photos!</li><br>
 				</ul>
-
 		</div><br><br>
 
 		<footer>
